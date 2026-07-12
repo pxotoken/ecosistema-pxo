5e5e372deae2e73b695597c6acee3da3f3e8aaed
eaclarich <eaclarich@gmail.com>
2026-07-12 15:10:56 -0300

    send/receive main wallet page

--- apps/web/src/components/crypto/index.ts
+++ apps/web/src/components/crypto/index.ts
@@ -1,3 +1,5 @@
 export { WalletModal } from './WalletModal';
 export { ConnectWalletButton } from './ConnectWalletButton';
-export { AccedePXOButton } from './AccedePXOButton'; 
+export { AccedePXOButton } from './AccedePXOButton';
+export { ReceiveFundsModal } from './ReceiveFundsModal';
+export { SendFundsModal } from './SendFundsModal';
